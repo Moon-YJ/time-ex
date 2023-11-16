40a83fd50ad3bec3b1e6101e15998cb5808b6986
Moon-YJ <tunjeong@gmail.com>
2023-11-16 10:56:15 +0900

    auto버튼 기능 추가

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ const main = document.querySelector('main');
 const numbers = main.querySelectorAll('.screen span');
 const em = main.querySelector('.screen .apm');
 const btns = main.querySelectorAll('nav span');
-let timer;
+const autoBtn = main.querySelector('button.auto');
 
 // 자주 바뀔만한 값을 전역변수로 객체를 배열로 묶어두는 형태로 따로 빼서 관리
 // 해당 값이 아래 함수에서 호출되도록 처리
@@ -14,15 +14,40 @@ const data = [
 	{ cond: new Date().getHours() >= 20 || new Date().getHours() < 5, name: 'night' },
 ];
 
-timer = setInterval(() => {
-	// data전역변수를 인수로 받아서 호출
-	changeTheme(data);
+// 1초마다 전자시계 출력하는 함수 호출
+// 특정함수에 콜백함수를 전달할때는 함수 호출 구문이 아닌 정의문 형태로 전달
+// setWatch처럼 함수명만 넣으면 정의형태이기 때문에 바로 등록 가능
+let timer1 = setInterval(setWatch, 1000);
+// changeTheme함수는 data 인수를 전달해야돼서 () 붙여야함
+// () 붙이는 순간, 정의형태가 아닌 호출형태로 변경되므로 다시 익명함수로 호출문을 wrapping해서 정의형태로 변경
+let timer2 = setInterval(() => changeTheme(data), 1000);
+
+btns.forEach((btn, idx) => {
+	btn.addEventListener('click', (e) => {
+		btns.forEach((btn) => btn.classList.remove('on'));
+		e.currentTarget.classList.add('on');
+		clearInterval(timer1);
+		clearInterval(timer2);
+		main.className = '';
+		main.classList.add(e.currentTarget.innerText.toLowerCase());
+		// toLowerCase() - 모든 문자를 소문자로 반환
+		// toUpperCase() - 모든 문자를 대문자로 반환
+	});
+});
+
+autoBtn.addEventListener('click', () => {
+	btns.forEach((btn) => btn.classList.remove('on'));
+	timer1 = setInterval(setWatch, 1000);
+	timer2 = setInterval(() => changeTheme(data), 1000);
+});
+
+function setWatch() {
 	em.innerText = new Date().getHours() < 12 ? 'am' : 'pm';
 	//getTime함수가 [시간,분,초]반환
 	//반환된 배열값을 그대로 반복돌면서 setTime함수에 인수로 전달
 	//setTime반복돌면서 시간,분,초에 1자리수일때 앞에 '0'을 붙여주는 공통로직 반복 처리
 	getTime().forEach((num, idx) => setTime(num, idx));
-}, 1000);
+}
 
 // 시간값을 구해서 반환하는 함수
 function getTime() {
@@ -65,15 +90,3 @@ function changeTheme(info) {
     }
   */
 }
-
-btns.forEach((btn, idx) => {
-	btn.addEventListener('click', (e) => {
-		btns.forEach((btn) => btn.classList.remove('on'));
-		e.currentTarget.classList.add('on');
-		clearInterval(timer);
-		main.className = '';
-		main.classList.add(e.currentTarget.innerText.toLowerCase());
-		// toLowerCase() - 모든 문자를 소문자로 반환
-		// toUpperCase() - 모든 문자를 대문자로 반환
-	});
-});
